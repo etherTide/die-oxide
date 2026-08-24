6dc6f595732c4a35721ccd15ac3764f0df528395
Ether <134476332+etherTide@users.noreply.github.com>
2026-08-24 21:59:43 +1000

    Implemented count and type of die, as well as advantage and disadvantage

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
+use clap::Parser;
 use rand::random_range;
-// use clap::Parser;
-//
-// #[derive(Parser, Debug)]
-// #[command(version, about, long_about = None)]
-// struct Args {
-//     /// Name of the person to greet
-//     #[arg(short, long)]
-//     name: String,
-//
-//     /// Number of times to greet
-//     #[arg(short, long, default_value_t = 1)]
-//     count: u8,
-// }
-//
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct Die {
     face_count: u8,
 }
@@ -22,30 +9,42 @@ impl Die {
     const fn new(face_count: u8) -> Self {
         Self { face_count }
     }
+
     fn roll(&self) -> u8 {
         random_range(1..=self.face_count)
     }
+
+    fn standard_set() -> [Die; 7] {
+        let dice: [u8; 7] = [4, 6, 8, 10, 12, 20, 100];
+        dice.map(|die| Die::new(die))
+    }
+}
+impl Default for Die {
+    fn default() -> Self {
+        Self::new(6)
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(default_value_t = 1)]
+    count: u8,
+
+    /// Kind of dice to roll
+    #[arg(short, default_value_t = 6)]
+    die: u8,
+
+    /// Roll die twice and take the higher result
+    #[arg(short, long)]
+    advantage: bool,
+
+    /// Roll die twice and take the lower result
+    #[arg(short = 'A', long)]
+    disadvantage: bool,
 }
 
 fn main() {
-    let standard_dice = [
-        Die::new(4),
-        Die::new(6),
-        Die::new(8),
-        Die::new(10),
-        Die::new(12),
-        Die::new(20),
-        Die::new(100),
-    ];
-    for die in standard_dice {
-        let result = die.roll();
-        println!("{die:?} => {result:?}");
-    }
-
-    // let args = Args::parse();
-
-    // if args.count == 0 {
-    //     interactive_mode();
-    //     return;
-    // }
+    let args = Args::parse();
+    println!("{}", Die::new(args.die).roll_multiple(args.count));
 }
