d606cc7c27e7d3dab344669de0d602be382336ca
Ether <134476332+etherTide@users.noreply.github.com>
2026-09-07 13:44:58 +1000

    Lints: applied Clippy suggestions

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,12 @@ use die_oxide::cli::{Cli, RollCommand};
 
 fn main() {
     let cli = Cli::parse();
-    match cli.roll_cmds {
-        Some(roll_cmds) => {
-            for cmd in roll_cmds {
-                cmd.print_roll();
-            }
-        }
-        None => {
-            let cmd = RollCommand::default();
+    if let Some(roll_cmds) = cli.roll_cmds {
+        for cmd in roll_cmds {
             cmd.print_roll();
         }
+    } else {
+        let cmd = RollCommand::default();
+        cmd.print_roll();
     }
 }
