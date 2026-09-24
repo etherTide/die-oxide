af297b36c59710223a56b0b9ca8264eb9121bd83
Ether <134476332+etherTide@users.noreply.github.com>
2026-09-24 19:42:29 +1000

    Update command help messages

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -6,10 +6,10 @@ use std::{rc::Rc, str::FromStr};
 #[command(version, about, long_about = None)]
 /// A command-line dice roller
 pub struct Cli {
-    /// [`count`] [d `die`] [+ | - `mod`]* ...
-    ///
-    /// e.g. 8d6-1+3 2d4+2+0-1 5 +2 d100 ...
-    /// default: 1d6+0
+    /// [count] [die] [mod]* ...
+    #[arg(
+        long_help = "[count: number] [die: 'd'number] [mod: ('+'|'-')num]*\ne.g. 8d6-1+3 2d4+2+0-1 5 +2 d100 ...\ndefault: 1d6+0"
+    )]
     pub roll_cmds: Option<Vec<RollCommand>>,
 }
 impl Cli {
